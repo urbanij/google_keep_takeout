b7bfca3aeb6eb1aa2e30abdee458cf2b77e5434e
Francesco Urbani <francescourbanidue@gmail.com>
2020-07-15 12:19:54 +0200

    notes sorted by date and archived/not archived

--- export.py
+++ export.py
@@ -6,47 +6,45 @@ Export Google Keep notes to a txt file.
 After obtaining your Google Keep data from https://takeout.google.com/
 unzip the folder and cd into it.
 
-Copy this file in that folder and run, e.g.
+Copy this file in that folder and run:
     python export.py > exported.txt
 
-, alternatively feel free to edit this file to achieve what fits best for you.
 
 """
 
-
-import json
-import sys
 import os
+import json
 import datetime 
 
 
 
 class Note:
-    """docstring for Note"""
     def __init__(self, filename, data):
         self._name = filename.replace('.json', '')
         self._title = data['title']
         self._raw_date = data['userEditedTimestampUsec']
         self._date = datetime.datetime.fromtimestamp(self._raw_date/1_000_000)
+        self._isTrashed = data['isTrashed']
+        self._isArchived = data['isArchived']
         self._isList = True if 'listContent' in data else False
-        if not self._isList:
-            self._content = data['textContent'] 
-        else:
+        if self._isList:
             tmp = ""
             for i in data['listContent']:
                 tick = '+' if i['isChecked'] == True else ' '
                 tmp += "- [{}] {}\n".format(tick, i['text'])
-            self._content = tmp
-
+            self._content = tmp; del tmp
+        else:
+            self._content = data['textContent'] 
 
     def _format_date(self):
-        # e.g.: Tuesday November 03, 2015, 03:20:51 PM
-        # tnx https://strftime.org/
-        return self._date.strftime("%A %B %d, %Y, %I:%M:%S %p")
+        # return a date of this type: Tuesday November 03, 2015, 03:20:51 PM
+        return self._date.strftime("%A %B %d, %Y, %I:%M:%S %p") # https://strftime.org/
+
+    def isTrashed(self):
+        return self._isTrashed
 
     def __repr__(self):
-        ans = ""
-        ans += "### {}\n".format(self._name)
+        ans  = "### {} {}\n".format(self._name, "(Archived)" if self._isArchived else "")
         ans += "Last edited: {}\n".format(self._format_date())
         if self._title != '': ans += "Title: {}\n".format(self._title)
         ans += self._content
@@ -58,21 +56,26 @@ class Note:
 if __name__ == '__main__':
 
     root_folder = 'Keep'
+    notes = [filename for filename in os.listdir(root_folder) if '.json' in filename]
 
-    notes = os.listdir(root_folder)
-    notes = [filename for filename in notes if '.json' in filename]
-
-        
     notes_list = []
     for filename in notes:
         with open("{}/{}".format(root_folder, filename), 'r') as json_file:
             data = json.load(json_file)
-
-        notes_list.append(Note(filename, data))
+            #print("{}\n\n".format(data))
+        note = Note(filename, data)
+        if not note.isTrashed():
+            notes_list.append(note)
 
     del notes
 
-    sorted_notes = sorted(notes_list, key=lambda x: x._raw_date, reverse=True)
 
+    #sorted_notes = sorted(notes_list, key=lambda x: x._raw_date, reverse=True)
+    sorted_notes = sorted(sorted(notes_list, key=lambda x : x._isArchived), key=lambda x : x._raw_date, reverse = True)  
+    
+
+
+    print("Total #notes = {}   -- sorted by (Last update, archived status)\n\n".format(len(sorted_notes)))
     [print("{}\n\n\n".format(i)) for i in sorted_notes]
+
 
